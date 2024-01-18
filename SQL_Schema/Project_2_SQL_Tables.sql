0c2580802d0eac101e6f84b7b5e7734546ea5e06
adrakevasquez <131911413+adrakevasquez@users.noreply.github.com>
2024-01-18 13:24:22 -0500

    Add files via upload

---
+++ SQL_Schema/Project_2_SQL_Tables.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS campaign; 
+DROP TABLE IF EXISTS contacts; 
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+
+CREATE TABLE contacts(
+	contact_id INT not null, 
+	first_name VARCHAR not null,
+	last_name VARCHAR not null,
+	email VARCHAR not null,
+		primary key (contact_id)
+);
+
+CREATE TABLE category(
+	category_id VARCHAR not null, 
+	category VARCHAR not null,
+	primary key(category_id)
+);
+
+CREATE TABLE subcategory(
+	subcategory_id VARCHAR not null, 	
+	subcategory VARCHAR not null, 
+	primary key (subcategory_id)
+);
+
+CREATE TABLE campaign (
+ cf_id INT not null,
+	contact_id INT not null,
+	company_name VARCHAR not null,
+	description VARCHAR not null,
+	goal INT not null,
+	pledged INT not null,
+	outcome VARCHAR not null,
+	backers_count INT not null,
+	country VARCHAR not null,
+	currency VARCHAR not null,	
+	launch_date VARCHAR not null,
+	end_date VARCHAR not null,
+	category_id VARCHAR not null,
+	subcategory_id VARCHAR not null,
+		primary key (cf_id),
+		foreign key (contact_id) references contacts(contact_id),
+		foreign key (category_id) references category(category_id),
+		foreign key (subcategory_id) references subcategory(subcategory_id)
+);
+
+select * from campaign;
